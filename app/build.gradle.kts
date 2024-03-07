a3dbf9daf0fc73423fc96ea70a2963dd3f30322e
jlsamson <113307718+jlsamson@users.noreply.github.com>
2024-03-06 19:49:49 -0700

    Removed accidental firebase implementation error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.firestore)
     implementation("android.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
