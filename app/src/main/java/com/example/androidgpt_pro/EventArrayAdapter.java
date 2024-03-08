eb8b5812dc879c8560ad44dfc6f9fabca97cfe83
paulicorise <112906815+paulicorise@users.noreply.github.com>
2024-03-07 18:09:54 -0700

    EventArrayAdapter

---
+++ app/src/main/java/com/example/androidgpt_pro/EventArrayAdapter.java
@@ -0,0 +1,47 @@
+package com.example.androidgpt_pro;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.bumptech.glide.Glide;
+import com.example.androidgpt_pro.R;
+import java.util.ArrayList;
+public class EventArrayAdapter extends ArrayAdapter<EventDatabaseControl>{
+    public EventArrayAdapter(Context context, ArrayList<EventDatabaseControl> events){
+        super(context,0,events);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View view;
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(R.layout.event_content_detail, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        EventDatabaseControl event = getItem(position);
+        TextView eventName = view.findViewById(R.id.event_name);
+        TextView eventDescription = view.findViewById(R.id.event_description);
+        TextView eventDate = view.findViewById(R.id.event_date);
+        TextView eventLocationApt = view.findViewById(R.id.event_location1);
+        TextView eventLocationCity = view.findViewById(R.id.event_location2);
+//        ImageView eventImage = view.findViewById(R.id.event_image);
+
+        // not sure...
+        String eventID = event.createEvent();
+
+        eventName.setText(event.getEventName(eventID));
+        eventDate.setText(event.getEventTime(eventID));
+        eventLocationApt.setText(event.getEventLocation(eventID));
+        eventLocationCity.setText(event.getEventSimplifiedLocation(eventID));
+        eventDescription.setText(event.getEventDescription(eventID));
+        return view;
+
+    }
+}
--- app/src/main/java/com/example/androidgpt_pro/EventBrowse.java
+++ app/src/main/java/com/example/androidgpt_pro/EventBrowse.java
@@ -3,5 +3,6 @@ package com.example.androidgpt_pro;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventBrowse extends AppCompatActivity {
+    // TODO: when the user click the event button in the Navigation bar, it jumps to this page with all events listed.
 
 }
