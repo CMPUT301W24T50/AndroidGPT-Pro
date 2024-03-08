3c3dd8123644a7924f52ef3f8228295a33338928
SATSTW <yizhi6@ualberta.ca>
2024-03-08 12:16:16 -0700

    Updated Database

--- app/src/main/java/com/example/androidgpt_pro/EventBrowseActivity.java
+++ app/src/main/java/com/example/androidgpt_pro/EventBrowseActivity.java
@@ -10,24 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import android.widget.ArrayAdapter;
 
 public class EventBrowseActivity extends AppCompatActivity {
     // TODO: when the user click the event button in the Navigation bar, it jumps to this page with all events listed.
-    private CollectionReference colRef = FirebaseFirestore
-            .getInstance()
-            .collection("Event");
     BottomNavigationView navigationTabs;
     private ArrayList<String> eventNames = new ArrayList<>();
     private ArrayAdapter<String> eventArrayAdapter;
@@ -35,26 +27,25 @@ public class EventBrowseActivity extends AppCompatActivity {
     private String eID;
 
     private void createSampleEvent() {
+        EventDatabaseControl edc = new EventDatabaseControl();
         // We're going to add a hardcoded ID for demonstration purposes
-        colRef.document("00000000").get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        EventDatabaseControl edc = new EventDatabaseControl();
-                        String eLastEventID = documentSnapshot.getString("eLastEventID");
-                        eID = edc.strAddOne(eLastEventID);
-                        HashMap<String, String> data = new HashMap<>();
-                        data.put("eLastEventID", eID);
-                        colRef.document("00000000").set(data);
-                        edc.initEvent(eID, "Sample Event",
-                                "123 Main St",
-                                "City Center",
-                                "This is a sample event.",
-                                "April 10, 2024 8:00 PM");
-                        eventNames.add("Sample Event");
-                        eventArrayAdapter.notifyDataSetChanged();
-                    }
-                });
+        edc.getEventStat()
+            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot doc) {
+                    EventDatabaseControl edc = new EventDatabaseControl();
+                    String lastEventID = edc.getLastEventID(doc);
+                    eID = edc.calculateNextEventID(lastEventID);
+                    edc.setEventStat(eID);
+                    edc.initEvent(eID, "Sample Event",
+                            "123 Main St",
+                            "City Center",
+                            "This is a sample event.",
+                            "April 10, 2024 8:00 PM");
+                    eventNames.add("Sample Event");
+                    eventArrayAdapter.notifyDataSetChanged();
+                }
+            });
     }
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState){
