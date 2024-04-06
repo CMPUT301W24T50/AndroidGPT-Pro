c0cbfc8014383e7c214bf22f524503eec69180e4
SATSTW <113562940+SATSTW@users.noreply.github.com>
2024-04-06 17:34:15 -0600

    Updated database

--- app/src/main/java/com/example/androidgpt_pro/EventDatabaseControl.java
+++ app/src/main/java/com/example/androidgpt_pro/EventDatabaseControl.java
@@ -113,7 +113,7 @@ public class EventDatabaseControl {
 
 
     /**
-     * This is the event deleter.
+     * This is an event deleter.
      * @param eventID
      * eventID: An ID of an event.
      */
@@ -121,9 +121,10 @@ public class EventDatabaseControl {
         getEventSnapshot(eventID).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot docSns) {
-                eColRef.document(eventID).delete();
+                ds.delSignUpAllProfileEvent(getEventAllSignUpProfiles(docSns), eventID);
+                ds.delCheckInAllProfileEvent(getEventAllCheckInProfiles(docSns), eventID);
                 ds.delOrganizedProfileEvent(getEventOrganizerID(docSns), eventID);
-                ds.delSignUpAllProfileEvent(eventID);
+                eColRef.document(eventID).delete();
             }
         });
     }
